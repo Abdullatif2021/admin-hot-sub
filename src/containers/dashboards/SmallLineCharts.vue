12816419b390d5983101413ede165c64e4bf1fa7
samawal <samawal.aljaramany@gmail.com>
2022-08-17 16:14:43 +0300

    reformat the dashboard, adding the needed components with some todos

---
+++ src/containers/dashboards/SmallLineCharts.vue
@@ -0,0 +1,45 @@
+<template>
+  <b-row>
+    <b-colxx xxs="12" class="mb-4">
+<!--      todo:needs fixed height-->
+      <vue-perfect-scrollbar
+        class="scroll dashboard-list-with-thumbs"
+        :settings="{ suppressScrollX: true, wheelPropagation: false }"
+      >
+        <small-line-chart-card :class="itemClass" label-prefix="$" :data="smallChartData1"/>
+        <small-line-chart-card :class="itemClass" label-prefix="$" :data="smallChartData2"/>
+        <small-line-chart-card :class="itemClass" label-prefix="$" :data="smallChartData3"/>
+        <small-line-chart-card :class="itemClass" label-prefix="$" :data="smallChartData4"/>
+        <small-line-chart-card :class="itemClass" label-prefix="$" :data="smallChartData1"/>
+        <small-line-chart-card :class="itemClass" label-prefix="$" :data="smallChartData2"/>
+        <small-line-chart-card :class="itemClass" label-prefix="$" :data="smallChartData3"/>
+        <small-line-chart-card :class="itemClass" label-prefix="$" :data="smallChartData4"/>
+      </vue-perfect-scrollbar>
+    </b-colxx>
+  </b-row>
+</template>
+<script>
+import SmallLineChartCard from "../../components/Cards/SmallLineChartCard";
+import {
+  smallChartData1,
+  smallChartData2,
+  smallChartData3,
+  smallChartData4
+} from "../../data/charts";
+
+
+export default {
+  components: {
+    "small-line-chart-card": SmallLineChartCard
+  },
+  props: ["itemClass"],
+  data() {
+    return {
+      smallChartData1,
+      smallChartData2,
+      smallChartData3,
+      smallChartData4
+    };
+  }
+};
+</script>
--- src/views/app/piaf/Start.vue
+++ src/views/app/piaf/Start.vue
@@ -2,41 +2,44 @@
   <div>
     <b-row>
       <b-colxx xxs="12">
-        <piaf-breadcrumb :heading="$t('menu.start')" />
+        <piaf-breadcrumb :heading="$t('menu.start')"/>
         <div class="separator mb-5"></div>
       </b-colxx>
     </b-row>
     <b-row>
       <b-colxx xxs="12">
-        <h5 class="mb-4 card-title">{{ $t("cards.icon-card") }}</h5>
-        <b-row class="icon-cards-row mb-3">
-          <b-colxx xxs="6" sm="4" md="3" lg="2">
-            <icon-card
-              :title="$t('dashboards.pending-orders')"
-              icon="iconsminds-clock"
-              :value="14"
-            />
+        <b-row>
+          <b-colxx lg="8" xl="8" class="mb-4">
+            <sortable-statictics-row></sortable-statictics-row>
+            <h4 class="mb-3">Active Auctions</h4>
+            <small-line-charts
+              itemClass="dashboard-small-chart"
+            ></small-line-charts>
+<!--            <top-rated-items></top-rated-items>-->
           </b-colxx>
-          <b-colxx xxs="6" sm="4" md="3" lg="2">
-            <icon-card
-              :title="$t('dashboards.completed-orders')"
-              icon="iconsminds-basket-coins"
-              :value="32"
-            />
+          <b-colxx lg="4" md="6" class="mb-4">
+            <logs class="mb-4"></logs>
+            <tickets></tickets>
           </b-colxx>
-          <b-colxx xxs="6" sm="4" md="3" lg="2">
-            <icon-card
-              :title="$t('dashboards.refund-requests')"
-              icon="iconsminds-arrow-refresh"
-              :value="74"
-            />
+        </b-row>
+        <b-row>
+          <b-colxx sm="12" lg="4" class="mb-4">
+            <profile-statuses></profile-statuses>
           </b-colxx>
-          <b-colxx xxs="6" sm="4" md="3" lg="2">
-            <icon-card
-              :title="$t('dashboards.new-comments')"
-              icon="iconsminds-mail-read"
-              :value="25"
-            />
+          <b-colxx md="6" lg="4" class="mb-4">
+            <gradient-card>
+              <b-badge pill variant="theme-3" class="align-self-start mb-3">{{
+                  $t("dashboards.piaf")
+                }}
+              </b-badge>
+              <p class="lead text-white">
+                {{ $t("dashboards.magic-is-in-the-details") }}
+              </p>
+              <p class="text-white">{{ $t("dashboards.yes-it-is-indeed") }}</p>
+            </gradient-card>
+          </b-colxx>
+          <b-colxx md="6" lg="4" class="mb-4">
+            <cakes></cakes>
           </b-colxx>
         </b-row>
       </b-colxx>
@@ -45,10 +48,29 @@
 </template>
 <script>
 import IconCard from "../../../components/Cards/IconCard.vue";
-import { mapActions } from "vuex";
+import GradientCard from "../../../components/Cards/GradientCard";
+import {mapActions} from "vuex";
+import SmallLineCharts from "@/containers/dashboards/SmallLineCharts";
+import TopRatedItems from "@/containers/dashboards/TopRatedItems";
+import ProfileStatuses from "@/containers/dashboards/ProfileStatuses";
+import Cakes from "@/containers/dashboards/Cakes";
+import Logs from "../../../containers/dashboards/Logs";
+import Tickets from "../../../containers/dashboards/Tickets";
+import RecentOrders from "@/containers/dashboards/RecentOrders";
+import SortableStaticticsRow from "@/containers/dashboards/SortableStaticticsRow";
+
 export default {
   components: {
-    "icon-card": IconCard
+    "icon-card": IconCard,
+    "small-line-charts": SmallLineCharts,
+    "top-rated-items": TopRatedItems,
+    cakes: Cakes,
+    logs: Logs,
+    tickets: Tickets,
+    "profile-statuses": ProfileStatuses,
+    "gradient-card": GradientCard,
+    "recent-orders": RecentOrders,
+    "sortable-statictics-row": SortableStaticticsRow,
   },
   data() {
     return {};
